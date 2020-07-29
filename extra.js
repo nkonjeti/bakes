c7efc95a7298e2c0cbc45658ab67b5a169417e2e
neha konjeti <nehakonjeti@nehas-air.lan>
2020-07-29 11:56:09 -0500

    tastyegglessbakes

--- extra.js
+++ extra.js
@@ -12,32 +12,32 @@
  };
  // Initialize Firebase
  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var firestore = firebase.firestore();
+ firebase.initializeApp(firebaseConfig);
+ var firestore = firebase.firestore();
 
-  const submitBtn = document.querySelector("#submit");
-  let userName = document.querySelector("#userFullName");
-  let userEmail = document.querySelector("#userEmail");
-  let userMessage = document.querySelector("#userMessage");
-const db = firestore.collection("contactData");
+ const submitBtn = document.querySelector("#submit");
+ let userName = document.querySelector("#userFullName");
+ let userEmail = document.querySelector("#userEmail");
+ let userMessage = document.querySelector("#userMessage");
+const db = firestore.collection("contactInfo");
 
 document.getElementById("submit").addEventListener("click", function(){
 //submitBtn.addEventListener("click", function() {
- let userNameInput = userName.value;
- let userEmailInput = userEmail.value;
- let userMessageInput = userMessage.value;
+let userNameInput = userName.value;
+let userEmailInput = userEmail.value;
+let userMessageInput = userMessage.value;
 
 //Access Database
 db.doc()
-  .set({
- name: userNameInput,
- email: userEmailInput,
- message: userMessageInput
+ .set({
+name: userNameInput,
+email: userEmailInput,
+message: userMessageInput
 })
 .then(function() {
- console.log("Data Saved");
-  })
+console.log("Data Saved");
+ })
 .catch(function(error) {
- console.log(error);
+console.log(error);
 });
 });
